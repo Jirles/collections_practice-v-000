0ede7489effb9bd9ab8fcdff3f2ebd7ef3f22daf
Emily Jirles <emily.jirles@gmail.com>
2018-04-28 14:43:37 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    if a == b 
+      0 
+    elsif a < b 
+      1 
+    else 
+      -1
+    end
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    if a.size == b.size 
+      0 
+    elsif a.size < b.size 
+      1 
+    else 
+      -1
+    end
+end
+
+def swap_elements(array)
+  second = array[1]
+  third = array[2]
+  array[1] = third
+  array[2] = second
+ end
